f055abe4f65c935fc758ce0cca612dd1ad034b35
= <=>
2025-01-30 12:16:07 +0530

    Song Info is working

--- src/components/songButton.jsx
+++ src/components/songButton.jsx
@@ -2,14 +2,18 @@ import PropTypes from 'prop-types';
 import '../styles.css';
 import './styles/playBar.css';
 import repeat from '../assets/SVGs/repeat.svg';
+import repeatOne from '../assets/SVGs/repeatOn.svg';
 import prev from '../assets/SVGs/previous.svg';
 import play1 from '../assets/SVGs/play1.svg';
 import next from '../assets/SVGs/next.svg';
 import shuffle from '../assets/SVGs/shuffle.svg';
+import shuffleOn from '../assets/SVGs/shuffleOn.svg';
 import pause from '../assets/SVGs/pause.svg';
 
  const SongButton = ({
     isPlaying, 
+    isShuffleOn,
+    isRepeatOn,
     togglePlayPause, 
     handleNext, 
     handlePrevious, 
@@ -19,7 +23,10 @@ import pause from '../assets/SVGs/pause.svg';
      return(
      <div className='song-button'>
      
-        <img id="repeat" src={repeat} alt="repeat" onClick = {handleRepeat} />
+        <img id="repeat" 
+            src={isRepeatOn ? repeatOne:repeat} 
+            alt={isRepeatOn ? "repeatOn":"repeat"}
+            onClick = {handleRepeat} />
         <img id="previous" src={prev} alt="previous" onClick = {handlePrevious} />
         <img 
             id="play" 
@@ -27,7 +34,10 @@ import pause from '../assets/SVGs/pause.svg';
             alt={isPlaying ? "pause":"play"}
             onClick = {togglePlayPause} />
         <img id="next" src={next} alt="next" onClick={handleNext} />
-        <img id="shuffle" src={shuffle} alt="shuffle" onClick={handleShuffle} />
+        <img id="shuffle" 
+            src={isShuffleOn ? shuffleOn:shuffle} 
+            alt={isShuffleOn ? "shuffleOn":"shuffle"} 
+            onClick={handleShuffle} />
     </div>
     
     );
@@ -35,6 +45,8 @@ import pause from '../assets/SVGs/pause.svg';
 
  SongButton.propTypes = {
     isPlaying: PropTypes.bool.isRequired,
+    isShuffleOn: PropTypes.bool.isRequired,
+    isRepeatOn: PropTypes.bool.isRequired,
     togglePlayPause: PropTypes.func.isRequired,
     handleNext: PropTypes.func.isRequired,
     handlePrevious: PropTypes.func.isRequired,
--- src/components/songInfo.jsx
+++ src/components/songInfo.jsx
@@ -1,9 +1,23 @@
+import PropTypes from 'prop-types';
 import '../styles.css';
-import './styles/playBar.css';
-const SongInfo = ()=>{
+import './styles/songInfo.css';
+const SongInfo = ({song})=>{
+    console.log("Current Song in PlayBar:", song);
+    if(!song || !song.name){
+        return null;
+    }
     return(
         <div className="song-info">
+            <h3 className='song-name'>{song.name.replace(/.mp3/, "").replace(/-/g, "by")} </h3>
         </div>
     );
 };
-export default SongInfo
+
+SongInfo.propTypes = {
+    song: PropTypes.shape({
+        name:PropTypes.string,
+        url:PropTypes.string,
+    }),
+};
+
+export default SongInfo;
